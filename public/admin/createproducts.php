e472b8ed9922d7681cad35301d8dae2c066e8d9b
Henkeullen <57135310+Henkeullen@users.noreply.github.com>
2020-05-28 14:57:29 +0200

    Bilder
    Nu fungerar det att ladda upp en bild, ändras dock när man lägger upp en till.
    
    Refaktuering på Signup & login påbörjad.

--- public/admin/createproducts.php
+++ public/admin/createproducts.php
@@ -31,13 +31,13 @@
     $price       = '';
     $error       = '';
     $msg         = '';
-    $img_url     = '';
+    // $img_url     = '';
     
     if (isset($_POST['add'])) {
         $title       = trim($_POST['title']);
         $description = trim($_POST['description']);
         $price       = trim($_POST['price']);
-        $img_url     = trim($_POST['img_url']);
+    // $img_url     = trim($_POST['img_url']);
         
         if (empty($title)) {
             $error .= "<p>Title is mandatory</p>";
@@ -59,15 +59,15 @@
 
             try {
                 $query = "
-                INSERT INTO products (title, description, price, img_url)
-                VALUES (:title, :description, :price, :img_url);
+                 INSERT INTO products (title, description, price)
+                 VALUES (:title, :description, :price);
                 ";
 
                 $stmt = $dbconnect->prepare($query);
                 $stmt->bindValue(':title', $title);
                 $stmt->bindValue(':description', $description);
                 $stmt->bindValue(':price', $price);
-                $stmt->bindValue(':img_url', $img_url);
+                // $stmt->bindValue(':img_url', $img_url);
                 $products = $stmt->execute();
             } catch (\PDOException $e) {
                 throw new \PDOException($e->getMessage(), (int) $e->getCode()); 
@@ -87,77 +87,74 @@
     }
 
     // UPLOAD IMAGE --->
-    //checking if the form has been submitted
-    // $msg            = "";
-    // $error          = "";
-    // $newPathAndName = "";
-    // $img_url        = '';
-    
+    // checking if the form has been submitted
+    $msg            = "";
+    $error          = "";
+    $newPathAndName = "";
+    $img_url        = "";
 
-    // if(isset($_POST['add'])){
+    if(isset($_POST['add'])){
 
-    //     // Validation for file upload starts here
-    //     if(is_uploaded_file($_FILES['uploadedFile']['tmp_name'])) {
-    //         //this is the actual name of the file
-    //         $fileName = $_FILES['uploadedFile']['name'];
-    //         //this is the file type
-    //         $fileType = $_FILES['uploadedFile']['type'];
-    //         //this is the temporary name of the file
-    //         $fileTempName = $_FILES['uploadedFile']['tmp_name'];
-    //         //this is the path where you want to save the actual file
-    //         $path = "../img/";
-    //         //this is the actual path and actual name of the file
-    //         $newPathAndName = $path . $fileName;
+        // Validation for file upload starts here
+        if(is_uploaded_file($_FILES['upload']['tmp_name'])) {
+            //this is the actual name of the file
+            $fileName = $_FILES['upload']['name'];
+            //this is the file type
+            $fileType = $_FILES['upload']['type'];
+            //this is the temporary name of the file
+            $fileTempName = $_FILES['upload']['tmp_name'];
+            //this is the path where you want to save the actual file
+            $path = "img/";
+            //this is the actual path and actual name of the file
+            $newPathAndName = $path . $fileName;
 
-    //         // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
-    //         // Check MIME Type by yourself.
-    //         $allowedFileTypes = [
-    //             'image/jpg',
-    //             'image/jpeg',
-    //             'image/gif',
-    //             'image/png',
-    //         ];
-    //   //       echo "<pre>";
-    //         // var_dump( (bool) array_search($fileType, $allowedFileTypes, true));
-    //         // echo "</pre>";
+            // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+            // Check MIME Type by yourself.
+            $allowedFileTypes = [
+                'image/jpg',
+                'image/jpeg',
+                'image/gif',
+                'image/png',
+            ];
+                echo "<pre>";
+                var_dump( (bool) array_search($fileType, $allowedFileTypes, true));
+                echo "</pre>";
 
-    //         $isFileTypeAllowed = (bool) array_search($fileType, $allowedFileTypes, true);
-    //         if ($isFileTypeAllowed == false) {
-    //             $error = "The file type is invalid. Allowed types are jpeg, png, gif.<br>";
-    //         } else {
-    //             // Will try to upload the file with the function 'move_uploaded_file'
-    //             // Returns true/false depending if it was successful or not
-    //             $isTheFileUploaded = move_uploaded_file($fileTempName, $newPathAndName);
-    //             if ($isTheFileUploaded == false) {
-    //                 // Otherwise, if upload unsuccessful, show errormessage
-    //                 $error = "Could not upload the file. Please try again<br>";
-    //             }
-    //         }
-    //     }
+            $isFileTypeAllowed = (bool) array_search($fileType, $allowedFileTypes, true);
+            if ($isFileTypeAllowed == false) {
+                $error = "The file type is invalid. Allowed types are jpeg, png, gif.<br>";
+            } else {
+                // Will try to upload the file with the function 'move_uploaded_file'
+                // Returns true/false depending if it was successful or not
+                $isTheFileUploaded = move_uploaded_file($fileTempName, $newPathAndName);
+                if ($isTheFileUploaded == false) {
+                    // Otherwise, if upload unsuccessful, show errormessage
+                    $error = "Could not upload the file. Please try again<br>";
+                }
+            }
+        }
 
-    //     // Handle the rest of the form validation and save accordingly in DB
-
-    //     if (empty($error)) {
-    //         $msg = "Succefully submittet the form";
-    //         // Save the image url in DB here, along with other data
-    //         $img_url = $newPathAndName;
-    //     } else {
-    //         $msg = $error;
-    //     }
-    // }
+        if (empty($error)) {
+            $msg = "Successfully uploaded the new rum";
+            // Save the image url in DB here, along with other data
+            $img_url = $newPathAndName;
+        } else {
+            $msg = $error;
+        }
+    }
 
 
 ?>
 	<div>
 		<form action="../edit.php?" method="GET">
-			<input type="hidden" name="id" value="<?=$_SESSION['id']?>">
 			<input type="submit" value="My page" class="btn">
 		</form>
 	</div>
     <form action="admin.php">
         <button class="contentBtn">Back</button>
     </form>
-
+<!-- Lägg till nya artiklar -->
+<!-- Lägg till nya artiklar -->
     <div class="box2">
         <div class="insidebox">
 
@@ -169,10 +166,10 @@
 
                     <br>
                         
-                <!-- <form action="products.php?" method="POST"> -->
-                <!-- file: <input type="file" name="uploadedFile" value=""/> -->
-                <!-- </form> -->
-                 <input type="text" name="img_url" id="add-img_url" placeholder="Bildens filnamn." >
+                    <form action="products.php?" method="POST">
+                    file: <input type="file" name="upload" value=""/> 
+                    </form> 
+                    <!-- <input type="text" name="img_url" id="add-img_url" placeholder="Bildens filnamn." > -->
 
                     <br>
 
@@ -189,14 +186,16 @@
             <?=$msg?>
         </div> 
     </div>
-    
+    <!-- Artiklar börjar här  -->
+    <!-- Artiklar börjar här  -->
     <div class="box">
         <ul class="lists">
             <?php foreach ($products as $key => $article) { ?>
-                <li class="blogOne">
+                <li class="articles">
 
-                    <img class="admin-image" src="<?=htmlentities(IMG_PATH . $article['img_url'])?>" alt="<?=htmlentities($article['title'])?>" style="height:200px; width:100px;">
-
+                    <div class="article_img">
+                        <img src="<?=$img_url?>">
+                   </div>
 
                     <h2>
                         <?=htmlentities($article['title'])?>
